db2aa959c8faf1c8f644502c21839af5dd0a480d
shaolang <shaolang@ymail.com>
2025-12-12 14:31:18 +0800

    Complete better assertions expect_that exercise

--- exercises/01_better_assertions/08_expect_that/src/lib.rs
+++ exercises/01_better_assertions/08_expect_that/src/lib.rs
@@ -1,14 +1,15 @@
 //! Use `expect_that!` to simplify `assert_that!` invocations and to check multiple properties on the same object.
 #[cfg(test)]
 mod tests {
-    use googletest::matchers::{all, each, empty, eq, ge, gt, lt, not};
-    use googletest::{assert_that, expect_that, verify_current_test_outcome};
+    use googletest::matchers::{each, empty, eq, ge, gt, lt, not};
+    use googletest::{expect_that, verify_current_test_outcome};
 
     #[googletest::gtest]
     fn multi_matcher() {
         let v: Vec<i32> = vec![1, 2, 3];
         // Convert the assertion below into two invocations of `expect_that!`.
-        assert_that!(v, all!(each(lt(&3)), each(gt(&1))))
+        expect_that!(v, each(lt(&3)));
+        expect_that!(v, each(gt(&1)));
     }
 
     #[googletest::gtest]
@@ -26,6 +27,9 @@ mod tests {
         };
 
         // Check that name and surname are not empty and that age is greater or equal than 18.
+        expect_that!(person.name, not(eq("")));
+        expect_that!(person.surname, not(eq("")));
+        expect_that!(person.age, ge(18));
     }
 
     #[googletest::gtest]
@@ -39,6 +43,8 @@ mod tests {
         //   and return an error.
         //   Do it without changing the assertions above!
         //   Tip: search for a verification function in googletest's docs.
+
+        verify_current_test_outcome()?;
 
         expect_that!(v, empty());
         Ok(())
