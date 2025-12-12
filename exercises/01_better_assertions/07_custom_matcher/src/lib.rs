41ebb2674b1643d919e667946ddccbbabcba2658
shaolang <shaolang@ymail.com>
2025-12-12 13:33:36 +0800

    Complete better assertions custom matcher exercise

--- exercises/01_better_assertions/07_custom_matcher/src/lib.rs
+++ exercises/01_better_assertions/07_custom_matcher/src/lib.rs
@@ -1,9 +1,33 @@
 //! Write a custom `is_redirect` matcher that checks if a `StatusCode` is a redirect.
-use googletest::matcher::Matcher;
+use googletest::{matcher::{Matcher, MatcherResult}, prelude::MatcherBase};
 use http::StatusCode;
 
 pub fn is_redirect() -> impl Matcher<StatusCode> {
-    todo!()
+    StatusCodeMatcher {
+        expected: StatusCode::MOVED_PERMANENTLY,
+    }
+}
+
+#[derive(MatcherBase)]
+struct StatusCodeMatcher {
+    expected: StatusCode,
+}
+
+impl Matcher<StatusCode> for StatusCodeMatcher {
+    fn matches(&self, actual: StatusCode) -> googletest::matcher::MatcherResult {
+        if self.expected == actual {
+            MatcherResult::Match
+        } else {
+            MatcherResult::NoMatch
+        }
+    }
+
+    fn describe(&self, matcher_result: googletest::matcher::MatcherResult) -> googletest::description::Description {
+        match matcher_result {
+            MatcherResult::Match => format!("is a redirection status code").into(),
+            MatcherResult::NoMatch => format!("isn't a redirection status code").into()
+        }
+    }
 }
 
 #[cfg(test)]
